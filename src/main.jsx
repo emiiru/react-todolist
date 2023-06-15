b608fa4d64ebbec25122835aac887c37b70783d7
Emil Tamayo <zen4emil@gmail.com>
2023-06-15 11:57:23 +0800

    Added React Router and Axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+import AboutPage from './pages/AboutPage.jsx'
+import BlogPage from './pages/BlogPage.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/about",
+    element: <AboutPage/>,
+  },
+  {
+    path: "/blogs",
+    element: <BlogPage/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import NavBar from '../components/NavBar'
+export default function AboutPage() {
+    return (
+        <>
+            <div className="container">
+            <NavBar className="mb-5" />
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>About Page</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
